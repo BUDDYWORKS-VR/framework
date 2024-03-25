5f3d338bec35dbec77fc6d496fec6f1de2d7c683
JustBuddy <8122264+JustBuddy@users.noreply.github.com>
2024-03-25 14:11:52 +0100

    2.0

---
+++ Editor/Import_v1.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace BUDDYWORKS.AvatarFramework.Importv1
+{
+    public class AvatarFramework : ScriptableObject
+    {
+        public const string packageName = "BUDDYWORKS";
+        
+        public static string[] excludeRegexs =
+        {
+            ".*\\.cs",
+            ".*\\.asmdef",
+            "Avatar Framework v2",
+            "package.json"
+        };
+
+        [MenuItem("BUDDYWORKS/Avatar Framework/Import Framework v1")]
+        public static void FancyPackage()
+        {
+            Type instancerType = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(x => x.GetType("BUDDYWORKS.AvatarFramework.Instancer.Instancer") != null)
+                .Select(x => x.GetType("BUDDYWORKS.AvatarFramework.Instancer.Instancer")).FirstOrDefault();
+
+            if (instancerType == null)
+            {
+                Debug.LogError("Instancer not found. To use this functionality, install the VRLabs Instancer from https://github.com/VRLabs/Instancer");    
+                return;
+            }
+
+            MethodInfo instanceMethod = instancerType.GetMethod("Instance", BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            if (instanceMethod == null)
+            {
+                Debug.LogError("Instance method not found");
+                return;
+            }
+            
+            var editor = ScriptableObject.CreateInstance<AvatarFramework>();
+            var script = MonoScript.FromScriptableObject(editor);
+            var assetPath =  (AssetDatabase.GetAssetPath(script));
+            
+            instanceMethod.Invoke(null, new object[] { packageName, assetPath, excludeRegexs });
+        }
+    }   
+}
---
+++ Editor/Instancer.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace BUDDYWORKS.AvatarFramework.Instancer
+{
+	public class Instancer : MonoBehaviour
+	{
+		public static string Instance(string packageName, string installFilePath, string[] excludeRegexs)
+		{
+			string targetFolder = EditorUtility.OpenFolderPanel("Select Directory To Copy Assets To", "Assets/", "");
+
+			if (targetFolder == "" || targetFolder == null)
+			{
+				Debug.LogError("No folder selected, please select a folder to copy the assets to.");
+				return null;
+			}
+
+			if (!targetFolder.Contains(Application.dataPath))
+			{
+				Debug.LogError("Selected folder is not in the Assets folder, please select a folder in the Assets directory.");
+				return null;
+			}
+
+			targetFolder = PrepareTargetFolderPath(targetFolder, packageName);
+			
+			string sourceFolder = GetSourceFolder(installFilePath);
+
+			string[] localAssetPaths = GetLocalAssetPaths(sourceFolder, excludeRegexs);
+
+			CreateDirectories(localAssetPaths, targetFolder);
+
+			CopyFiles(localAssetPaths, sourceFolder, targetFolder);
+
+			AssetDatabase.Refresh();
+
+			FixReferences(localAssetPaths, sourceFolder, targetFolder);
+			
+			AssetDatabase.Refresh();
+			
+			return targetFolder;
+		}
+		
+		public static void Install(string packageName, string installFilePath, string[] excludeRegexs, Action<string> callBack)
+		{
+			string instancePath = Instance(packageName, installFilePath, excludeRegexs);
+			callBack(instancePath);
+		}
+
+		static string PrepareTargetFolderPath(string folderPath, string packageName)
+		{
+			folderPath = "Assets" + folderPath.Remove(0, Application.dataPath.Length) + "/" + packageName;
+
+			if (Directory.Exists(folderPath))
+			{
+				int i = 1;
+				while (Directory.Exists(folderPath + i.ToString()))
+				{
+					i++;
+				}
+				
+				folderPath += i;
+			}
+
+			Directory.CreateDirectory(folderPath);
+			AssetDatabase.ImportAsset(folderPath);
+			return folderPath;
+		}
+		
+		static string GetSourceFolder(string installFilePath)
+		{
+			string sourceFolder = installFilePath;
+
+			while (!File.Exists(sourceFolder + "/package.json"))
+			{
+				sourceFolder = Path.GetDirectoryName(sourceFolder);
+			}
+
+			return sourceFolder.Replace("\\", "/");
+		}
+
+		static string[] GetLocalAssetPaths(string sourceFolder, string[] excludeRegexs)
+		{
+			string[] assetPaths = AssetDatabase.FindAssets("", new [] { sourceFolder }).Select(AssetDatabase.GUIDToAssetPath).ToArray();
+
+			string[] filteredLocalAssetPaths = assetPaths
+				.Select(path => path.Remove(0,sourceFolder.Length))
+				.Where(path => excludeRegexs.All(regex => !Regex.Match(path, regex).Success))
+				.ToArray();
+
+			return filteredLocalAssetPaths;
+		}
+		
+		static void CreateDirectories(string[] filePaths, string targetFolder)
+		{
+			try
+			{
+				AssetDatabase.StartAssetEditing();
+				foreach (string path in filePaths)
+				{
+					string targetPath = Path.GetDirectoryName(targetFolder + path);
+					if (!Directory.Exists(targetPath))
+					{
+						Directory.CreateDirectory(targetPath);
+						AssetDatabase.ImportAsset(targetPath);
+					}
+				}
+			}
+			finally
+			{
+				AssetDatabase.StopAssetEditing();
+			}
+		}
+
+		static void CopyFiles(string[] filePaths, string sourceFolder, string targetFolder)
+		{
+			try
+			{
+				AssetDatabase.StartAssetEditing();
+				foreach (string path in filePaths)
+				{
+					if (!Directory.Exists(sourceFolder + path))
+					{
+						AssetDatabase.CopyAsset(sourceFolder + path, targetFolder + path);
+					}
+				}
+			}
+			finally{
+				AssetDatabase.StopAssetEditing();
+			}
+		}
+
+		static void FixReferences(string[] localAssetPaths, string sourceFolder, string targetFolder)
+		{
+			foreach (string localAssetPath in localAssetPaths)
+			{
+				string targetAssetPath = targetFolder + localAssetPath;
+				UnityEngine.Object[] targetAssets = AssetDatabase.LoadAllAssetsAtPath(targetAssetPath);
+				foreach (var targetAsset in targetAssets)
+				{
+					SerializedObject serializedObject = new SerializedObject(targetAsset);
+					SerializedProperty property = serializedObject.GetIterator();
+					do
+					{
+						if (property.propertyType == SerializedPropertyType.ObjectReference)
+						{
+							// Debug.Log($"O{property.name}, {property.displayName}, {property.objectReferenceValue}");
+							if (property.objectReferenceValue != null)
+							{
+								property.objectReferenceValue = GetTargetVersion(sourceFolder, targetFolder, property.objectReferenceValue);
+							}
+						}
+
+						if (property.propertyType == SerializedPropertyType.ExposedReference)
+						{
+							// Debug.Log($"E{property.name}, {property.displayName}, {property.exposedReferenceValue}");
+							if (property.exposedReferenceValue != null)
+							{
+								property.exposedReferenceValue = GetTargetVersion(sourceFolder, targetFolder, property.exposedReferenceValue);
+							}
+						}
+					} while (property.Next(true));
+				
+					serializedObject.ApplyModifiedProperties();	
+				}
+			}
+		}
+
+		private static Object GetTargetVersion(string sourceFolder, string targetFolder, Object target)
+		{
+			string targetPath = AssetDatabase.GetAssetPath(target);
+			if (targetPath.StartsWith(sourceFolder))
+			{
+				string newTargetPath = targetFolder + targetPath.Remove(0, sourceFolder.Length); 
+				return AssetDatabase.LoadAllAssetsAtPath(newTargetPath).Where(obj => obj.GetType() == target.GetType()).FirstOrDefault(x => x.name == target.name);
+			}
+
+			return target;
+		}
+	}
+}
